f08fde083806fe9902ac790fb1eaae883420b18d
trieuconcrete <trieu@concrete.com>
2020-06-30 15:03:19 +0700

    update code

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.db.models import Q
+from django.http import HttpResponse
 
 from apps.blog.models import BlogPost
 from apps.blog.forms import CreateBlogPostForm, UpdateBlogPostForm
@@ -42,6 +43,10 @@ def edit_blog_view(request, slug):
         return redirect('must_authenticate')
 
     blog_post = get_object_or_404(BlogPost, slug=slug)
+
+    if blog_post.author != user:
+        return HttpResponse("You are not the auther of that post!")
+
     if request.POST:
         form = UpdateBlogPostForm(request.POST or None, request.FILES or None, instance=blog_post)
         if form.is_valid():
