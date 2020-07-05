97f26324cc75dd150dcb1fe6f6645e5bdf492059
trieuconcrete <trieu@concrete.com>
2020-07-05 12:06:11 +0700

    add rest framework

---
+++ apps/blog/api/views.py
@@ -0,0 +1,20 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+from apps.account.models import Account
+from apps.blog.models import BlogPost
+from apps.blog.api.serializers import BlogPostSerializer
+
+
+@api_view(['GET'])
+def api_detail_blog_view(request, slug):
+
+    try:
+        blog_post = BlogPost.objects.get(slug=slug)
+    except BlogPost.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == "GET":
+        serializer = BlogPostSerializer(blog_post)
+        return Response(serializer.data)
